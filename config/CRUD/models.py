f06f82e688d16e402454e9691fa974c073e73ab4
Szade <wojciech.szade@gmail.com>
2023-10-18 11:41:12 +0200

    Added basic validation and more details to models

--- config/CRUD/models.py
+++ config/CRUD/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 # Create your models here.
 class Product(models.Model):
-    name = models.CharField(max_length=255)
-    price = models.FloatField()
-    stock = models.IntegerField()
-    image_url = models.CharField(max_length=2083)
+    name = models.CharField(max_length = 255, null = False, blank = False)
+    price = models.FloatField(blank = True, validators=[MinValueValidator(0.0)])
+    stock = models.IntegerField(blank = True)
+    image_url = models.CharField(max_length = 2083, blank = True)
