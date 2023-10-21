fb25f0012f5a9854bf51c8b14e22b2f6f081182c
Szade <wojciech.szade@gmail.com>
2023-10-21 14:01:54 +0200

    Added Category and Promotion classes with base testes

--- config/CRUD/models.py
+++ config/CRUD/models.py
@@ -2,9 +2,24 @@ from django.db import models
 from django.core.validators import MinValueValidator
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length = 255, null = False, blank = False)
+    description = models.CharField(max_length = 5000, blank=True)
+    
+    
+class Promotion(models.Model):
+    name = models.CharField(max_length = 255, null = False, blank = False)
+    description = models.CharField(max_length = 5000, blank=True)
+    discount = models.FloatField(blank = True, validators=[MinValueValidator(0.0)])
+    products = models.ManyToManyField('Product', blank = True, symmetrical=False, related_name='promotions')
+
+
 class Product(models.Model):
     name = models.CharField(max_length = 255, null = False, blank = False)
     price = models.FloatField(blank = True, validators=[MinValueValidator(0.0)])
     stock = models.IntegerField(blank = True)
     image_url = models.CharField(max_length = 2083, blank = True)
     description = models.CharField(max_length = 5000, blank=True)
+    category = models.ForeignKey(Category, on_delete = models.CASCADE) # If category is deleted, delete the product
+    promotion = models.ForeignKey(Promotion, on_delete = models.SET_NULL, blank = True, null = True) # If promotion is deleted, set promotion to null
+    
--- config/CRUD/tests.py
+++ config/CRUD/tests.py
@@ -1,11 +1,12 @@
 from django.test import TestCase
-from .models import Product
+from .models import Product, Category, Promotion
 
 # Create your tests here.
 class CreateProductTests(TestCase):
     @classmethod
     def setUpClass(self):       # setUpClass is called once at the beginning of the test run (setUp is called before each test)
         self.products = []
+        self.category = Category.objects.create(name = "DefaultCategory", description = "DefaultDescription")
     
     @classmethod
     def tearDownClass(self):         # tearDown is called once at the end of the test run (tearDownClass is called after each test)
@@ -17,20 +18,63 @@ class CreateProductTests(TestCase):
             "name": "TestProduct",
             "price": 10.0,
             "stock": 10,
-            "image_url": "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-math-90946.jpg&fm=jpg"
+            "image_url": "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-math-90946.jpg&fm=jpg",
+            "category": self.category
         }
-        self.products.append(Product.objects.create(name = newProductDetails["name"], price = newProductDetails["price"], stock = newProductDetails["stock"], image_url = newProductDetails["image_url"]))
+        self.products.append(Product.objects.create(name = newProductDetails["name"], price = newProductDetails["price"], stock = newProductDetails["stock"], image_url = newProductDetails["image_url"], category=newProductDetails["category"]))
         self.assertEqual(Product.objects.get(name = newProductDetails["name"]).name, newProductDetails["name"])
+        
+    def testCreateProductWithCategory(self):
+        newCategoryDetails = {
+            "name": "TestCategory",
+            "description": "TestDescription"
+        }
+        category = Category.objects.create(name = newCategoryDetails["name"], description = newCategoryDetails["description"])
+        newProductDetails = {
+            "name": "TestProduct",
+            "price": 10.0,
+            "stock": 10,
+            "image_url": "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-math-90946.jpg&fm=jpg",
+            "category": category
+        }
+        self.products.append(Product.objects.create(name = newProductDetails["name"], price = newProductDetails["price"], stock = newProductDetails["stock"], image_url = newProductDetails["image_url"], category = newProductDetails["category"]))
+        self.assertEqual(Product.objects.get(name = newProductDetails["name"]).category.name, category.name)
+        # teardown
+        category.delete()
+        
+    def testCreateProductWithPromotion(self):
+        newPromotionDetails = {
+            "name": "TestPromotion",
+            "description": "TestDescription",
+            "discount": 0.5
+        }
+        promotion = Promotion.objects.create(name = newPromotionDetails["name"], description = newPromotionDetails["description"], discount = newPromotionDetails["discount"])
+        newProductDetails = {
+            "name": "TestProduct",
+            "price": 10.0,
+            "stock": 10,
+            "image_url": "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-math-90946.jpg&fm=jpg",
+            "promotion": promotion,
+            "category": self.category
+        }
+        self.products.append(Product.objects.create(name = newProductDetails["name"], price = newProductDetails["price"], stock = newProductDetails["stock"], image_url = newProductDetails["image_url"], promotion = newProductDetails["promotion"], category = newProductDetails["category"]))
+        self.assertEqual(Product.objects.get(name = newProductDetails["name"]).promotion.name, promotion.name)
+        # teardown
+        promotion.delete()
+        
+        
 
 
 class ReadProductTests(TestCase):
     @classmethod
     def setUpClass(self):
-        self.product = Product.objects.create(name = "TestProduct", price = 10.0, stock = 10, image_url = "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-math-90946.jpg&fm=jpg")
+        self.category = Category.objects.create(name = "TestCategory", description = "TestDescription")
+        self.product = Product.objects.create(name = "TestProduct", price = 10.0, stock = 10, image_url = "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-math-90946.jpg&fm=jpg", category=self.category)
         
     @classmethod
     def tearDownClass(self):
         self.product.delete()
+        self.category.delete()
         
     def testReadBasicProduct(self):
         self.assertEqual(Product.objects.get(id = self.product.id).name, "TestProduct")
@@ -42,11 +86,13 @@ class ReadProductTests(TestCase):
 class UpdateProductTests(TestCase):
     @classmethod
     def setUpClass(self):
-        self.product = Product.objects.create(name = "TestProduct", price = 10.0, stock = 10, image_url = "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-math-90946.jpg&fm=jpg")
+        self.category = Category.objects.create(name = "TestCategory", description = "TestDescription")
+        self.product = Product.objects.create(name = "TestProduct", price = 10.0, stock = 10, image_url = "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-math-90946.jpg&fm=jpg", category=self.category)
         
     @classmethod
     def tearDownClass(self):
         self.product.delete()
+        self.category.delete()
         
     def testUpdateBasicProduct(self):
         self.product.name = "NewTestProduct"
@@ -62,12 +108,17 @@ class UpdateProductTests(TestCase):
 class DeleteProductTests(TestCase):
     @classmethod
     def setUpClass(self):
-        self.product = Product.objects.create(name = "TestProduct", price = 10.0, stock = 10, image_url = "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-math-90946.jpg&fm=jpg")
+        self.category = Category.objects.create(name = "TestCategory", description = "TestDescription")
+        self.product = Product.objects.create(name = "TestProduct", price = 10.0, stock = 10, image_url = "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-math-90946.jpg&fm=jpg", category=self.category)
         
     @classmethod
     def tearDownClass(self):
         try:
             self.product.delete()
+        except:
+            pass
+        try:
+            self.category.delete()
         except:
             pass
     
