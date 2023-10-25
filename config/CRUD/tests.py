1e6a895a044c317a1dfbd6b705a230cb452ef3c7
Krystian Sereda <ksereda773@gmail.com>
2023-10-25 22:19:05 +0200

    after merge

--- config/CRUD/tests.py
+++ config/CRUD/tests.py
@@ -1,67 +1,237 @@
+import unittest
+
+from django.db.utils import IntegrityError
 from django.test import TestCase
 from .models import Product, Category, Promotion
 
 # Create your tests here.
 class CreateProductTests(TestCase):
+
     @classmethod
-    def setUpClass(self):       # setUpClass is called once at the beginning of the test run (setUp is called before each test)
+    def setUpClass(self):
         self.products = []
-        self.category = Category.objects.create(name = "DefaultCategory", description = "DefaultDescription")
-    
-    @classmethod
-    def tearDownClass(self):         # tearDown is called once at the end of the test run (tearDownClass is called after each test)
-        for product in self.products:
-            product.delete()
-    
-    def testCreateBasicProduct(self):
-        newProductDetails = {
-            "name": "TestProduct",
-            "price": 10.0,
-            "stock": 10,
-            "image_url": "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-math-90946.jpg&fm=jpg",
-            "category": self.category
-        }
-        self.products.append(Product.objects.create(name = newProductDetails["name"], price = newProductDetails["price"], stock = newProductDetails["stock"], image_url = newProductDetails["image_url"], category=newProductDetails["category"]))
-        self.assertEqual(Product.objects.get(name = newProductDetails["name"]).name, newProductDetails["name"])
-        
-    def testCreateProductWithCategory(self):
         newCategoryDetails = {
             "name": "TestCategory",
             "description": "TestDescription"
         }
-        category = Category.objects.create(name = newCategoryDetails["name"], description = newCategoryDetails["description"])
-        newProductDetails = {
-            "name": "TestProduct",
-            "price": 10.0,
-            "stock": 10,
-            "image_url": "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-math-90946.jpg&fm=jpg",
-            "category": category
-        }
-        self.products.append(Product.objects.create(name = newProductDetails["name"], price = newProductDetails["price"], stock = newProductDetails["stock"], image_url = newProductDetails["image_url"], category = newProductDetails["category"]))
-        self.assertEqual(Product.objects.get(name = newProductDetails["name"]).category.name, category.name)
-        # teardown
-        category.delete()
-        
-    def testCreateProductWithPromotion(self):
+        self.category = Category.objects.create(**newCategoryDetails)
+
         newPromotionDetails = {
             "name": "TestPromotion",
             "description": "TestDescription",
-            "discount": 0.5
+            "discount": 33.33
         }
-        promotion = Promotion.objects.create(name = newPromotionDetails["name"], description = newPromotionDetails["description"], discount = newPromotionDetails["discount"])
+        self.promotion = Promotion.objects.create(**newPromotionDetails)
+
+    @classmethod
+    def tearDownClass(self):
+        for product in self.products:
+            product.delete()
+        self.category.delete()
+        self.promotion.delete()
+
+    def testCreateProductWithoutNameShouldRaiseException(self):
+        newProductDetails = {
+            "name": None,
+            "price": 10.0,
+            "stock": 10,
+            "image_url": "https://example.pl/img=2137",
+            "description": "TestDescription",
+            "promotion_id": self.promotion.id,
+            "category_id": self.category.id
+        }
+
+        with self.assertRaises(IntegrityError):
+            Product.objects.create(**newProductDetails)
+
+    def testCreateProductWithoutPriceShouldRaiseException(self):
+        newProductDetails = {
+            "name": "TestProduct",
+            "price": None,
+            "stock": 10,
+            "image_url": "https://example.pl/img=2137",
+            "description": "TestDescription",
+            "promotion_id": self.promotion.id,
+            "category_id": self.category.id
+        }
+
+        with self.assertRaises(IntegrityError):
+            Product.objects.create(**newProductDetails)
+
+    def testCreateProductWithoutStockShouldRaiseException(self):
+        newProductDetails = {
+            "name": "TestProduct",
+            "price": 10.0,
+            "stock": None,
+            "image_url": "https://example.pl/img=2137",
+            "description": "TestDescription",
+            "promotion_id": self.promotion.id,
+            "category_id": self.category.id
+        }
+
+        with self.assertRaises(IntegrityError):
+            Product.objects.create(**newProductDetails)
+
+    def testCreateProductWithoutImageUrlShouldRaiseException(self):
         newProductDetails = {
             "name": "TestProduct",
             "price": 10.0,
             "stock": 10,
-            "image_url": "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-math-90946.jpg&fm=jpg",
-            "promotion": promotion,
-            "category": self.category
+            "image_url": None,
+            "description": "TestDescription",
+            "promotion_id": self.promotion.id,
+            "category_id": self.category.id
         }
-        self.products.append(Product.objects.create(name = newProductDetails["name"], price = newProductDetails["price"], stock = newProductDetails["stock"], image_url = newProductDetails["image_url"], promotion = newProductDetails["promotion"], category = newProductDetails["category"]))
-        self.assertEqual(Product.objects.get(name = newProductDetails["name"]).promotion.name, promotion.name)
-        # teardown
-        promotion.delete()
-        
+
+        with self.assertRaises(IntegrityError):
+            Product.objects.create(**newProductDetails)
+
+    def testCreateProductWithoutDescriptionShouldRaiseException(self):
+        newProductDetails = {
+            "name": "TestProduct",
+            "price": 10.0,
+            "stock": 10,
+            "image_url": "https://example.pl/img=2137",
+            "description": None,
+            "promotion_id": self.promotion.id,
+            "category_id": self.category.id
+        }
+
+        with self.assertRaises(IntegrityError):
+            Product.objects.create(**newProductDetails)
+
+    def testCreateProductWithoutCategoryShouldRaiseException(self):
+        newProductDetails = {
+            "name": "TestProduct",
+            "price": 10.0,
+            "stock": 10,
+            "image_url": "https://example.pl/img=2137",
+            "description": "TestDescription",
+            "promotion_id": self.promotion.id,
+            "category_id": None
+        }
+
+        with self.assertRaises(IntegrityError):
+            Product.objects.create(**newProductDetails)
+
+    def testCreateProductWithoutPromotionIdShouldCorrectlyCreate(self):
+        newProductDetails = {
+            "name": "TestProduct",
+            "price": 10.0,
+            "stock": 10,
+            "image_url": "https://example.pl/img=2137",
+            "description": "TestDescription",
+            "promotion_id": None,
+            "category_id": self.category.id
+        }
+
+        created_product = Product.objects.create(**newProductDetails)
+
+        expected_product = Product.objects.get(name=newProductDetails["name"])
+        for field in newProductDetails:
+            self.assertEqual(getattr(created_product, field), getattr(expected_product, field))
+
+    def testCreateCategoryWithoutNameShouldRaiseException(self):
+        newCategoryDetails = {
+            "name": None,
+            "description": "TestDescription"
+        }
+
+        with self.assertRaises(IntegrityError):
+            Category.objects.create(**newCategoryDetails)
+
+    def testCreateCategoryWithoutDescriptionShouldRaiseException(self):
+        newCategoryDetails = {
+            "name": "TestCategory",
+            "description": None
+        }
+
+        with self.assertRaises(IntegrityError):
+            Category.objects.create(**newCategoryDetails)
+
+    def testCreatePromotionWithoutNameShouldRaiseException(self):
+        newPromotionDetails = {
+            "name": None,
+            "description": "TestDescription",
+            "discount": 12.2
+        }
+
+        with self.assertRaises(IntegrityError):
+            Promotion.objects.create(**newPromotionDetails)
+
+    def testCreatePromotionWithoutDescriptionShouldRaiseException(self):
+        newPromotionDetails = {
+            "name": "TestName",
+            "description": None,
+            "discount": 12.2
+        }
+
+        with self.assertRaises(IntegrityError):
+            Promotion.objects.create(**newPromotionDetails)
+
+    def testCreatePromotionWithoutDiscountShouldRaiseException(self):
+        newPromotionDetails = {
+            "name": "TestName",
+            "description": "TestDescription",
+            "discount": None
+        }
+
+        with self.assertRaises(IntegrityError):
+            Promotion.objects.create(**newPromotionDetails)
+
+    @unittest.skip('todo: walidacja ujemnych liczb')
+    def testCreatePromotionWhenInvalidDiscountShouldRaiseException(self):
+        newPromotionDetails = {
+            "name": "TestName",
+            "description": "TestDescription",
+            "discount": -10
+        }
+
+        with self.assertRaises(IntegrityError):
+            Promotion.objects.create(**newPromotionDetails)
+
+    def testCreateValidProduct(self):
+        newProductDetails = {
+            "name": "TestProduct",
+            "price": 10.0,
+            "stock": 10,
+            "image_url": "https://example.pl/img=2137",
+            "description": "TestDescription",
+            "promotion_id": self.promotion.id,
+            "category_id": self.category.id
+        }
+
+        created_product = Product.objects.create(**newProductDetails)
+
+        expected_product = Product.objects.get(name=newProductDetails["name"])
+        for field in newProductDetails:
+            self.assertEqual(getattr(created_product, field), getattr(expected_product, field))
+
+    def testCreateValidCategory(self):
+        newCategoryDetails = {
+            "name": "TestCategory1",
+            "description": "TestDescription"
+        }
+
+        created_product = Category.objects.create(**newCategoryDetails)
+
+        expected_product = Category.objects.get(name=newCategoryDetails["name"])
+        for field in newCategoryDetails:
+            self.assertEqual(getattr(created_product, field), getattr(expected_product, field))
+
+    def testCreateValidPromotion(self):
+        newPromotionDetails = {
+            "name": "TestPromotion1",
+            "description": "TestDescription",
+            "discount": 33.33
+        }
+        created_product = Promotion.objects.create(**newPromotionDetails)
+
+        expected_product = Promotion.objects.get(name=newPromotionDetails["name"])
+        for field in newPromotionDetails:
+            self.assertEqual(getattr(created_product, field), getattr(expected_product, field))
+
+
         
 
 
