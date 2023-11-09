3a5bf9b2ef73c800f76c7247981b97f095b56262
Szade <96198418+WojciechSzade@users.noreply.github.com>
2023-11-09 13:16:23 +0100

    Update tests.py

--- config/CRUD/tests.py
+++ config/CRUD/tests.py
@@ -18,7 +18,7 @@ class CreateProductTests(TestCase):
         newPromotionDetails = {
             "name": "TestPromotion",
             "description": "TestDescription",
-            "discount": 33.34
+            "discount": 33.3
         }
         self.promotion = Promotion.objects.create(**newPromotionDetails)
 
